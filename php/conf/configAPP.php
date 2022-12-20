3da18ddab43f22b3917729322418ddd950d19e13
kiredsolutions <105985102+kiredsolutions@users.noreply.github.com>
2022-12-19 20:57:50 -0600

    copo de nieve

--- php/conf/configAPP.php
+++ php/conf/configAPP.php
@@ -2,14 +2,22 @@
     /*
         Parametros de conexión a la DB
     */
+
+    //DATOS DE CONEXION DEL CLIENTE
 	date_default_timezone_set('America/Tegucigalpa');
     const SERVERURL = "http://localhost/githubVieraflores/";
 	const SERVEREMPRESA = "Viera Flores y Asociados";
 	const SERVER = "localhost";
-    const DB = "clinicarehn_clinicare_claudiaflores";
+    const DB = "clinicarehn_clientes_vierayasociados";
     const USER = "clinicarehn_clinicare";
-    const PASS = "Cl|n1c@r32022#%.";
+    const PASS = "Clin1c@r32022#%.";
     const PRODUCT_PATH = "/githubVieraflores/upload/";
+
+    //DATOS DE CONEXION SERVIDOR PRINCIPAL
+    const SERVER_MAIN = "localhost";
+    const DB_MAIN = "clinicarehn_fayad_clinicare";
+    const USER_MAIN = "clinicarehn_clinicare";
+    const PASS_MAIN = "Clin1c@r32022#%.";        
 
     /*
         Para encrptar y Desencriptar
